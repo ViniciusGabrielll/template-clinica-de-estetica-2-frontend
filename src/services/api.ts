105a455cab422310d67b96af9b49898bfd86b950
Vinícius Gabriel <viniciusgabriel102009@gmail.com>
2026-09-10 18:19:45 -0300

    feat: add API_URL in host

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000/api";
+const API_URL = "https://template-clinica-de-estetica-backend-production.up.railway.app";
 
 export interface Service {
     id: number;
